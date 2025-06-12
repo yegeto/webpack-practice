30a710dba130d5970f35acedb330cb5b841f825f
yegeto <115154804+yegeto@users.noreply.github.com>
2025-06-12 10:20:28 +0300

    update webpack.config.js / add odin.png

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import "./styles.css";
 import { greeting } from "./greeting.js";
+import odinImage from "./odin.png";
+
+const image = document.createElement("img");
+image.src = odinImage;
+
+document.body.appendChild(image);
 
 console.log(greeting);
